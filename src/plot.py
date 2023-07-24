155c78bcb0ea2e4ff20c3cbd447ea147f8ab358c
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2023-07-24 14:59:32 -0700

    Incorporate UK estimates into plots

--- src/plot.py
+++ src/plot.py
@@ -15,10 +15,12 @@ coronasurveys_eng = pd.read_csv('data/filtered/coronasurveys-eng.csv')
 coronasurveys_nir = pd.read_csv('data/filtered/coronasurveys-nir.csv')
 coronasurveys_sct = pd.read_csv('data/filtered/coronasurveys-sct.csv')
 coronasurveys_wls = pd.read_csv('data/filtered/coronasurveys-wls.csv')
+coronasurveys_gb = pd.read_csv('data/filtered/coronasurveys-gb.csv')
 ons_eng = pd.read_csv('data/filtered/ons-eng.csv')
 ons_nir = pd.read_csv('data/filtered/ons-nir.csv')
 ons_sct = pd.read_csv('data/filtered/ons-sct.csv')
 ons_wls = pd.read_csv('data/filtered/ons-wls.csv')
+ons_gb = pd.read_csv('data/filtered/ons-gb.csv')
 
 # plot comparisons
 def coronasurveys():
@@ -71,6 +73,18 @@ def coronasurveys():
     coronasurveys_wls['p_cases_high'], # type: ignore
     alpha=0.5,
     label='Wales (error)'
+  )
+  plt.plot(
+    coronasurveys_gb['days'],
+    coronasurveys_gb['p_active'],
+    label='UK, active'
+  )
+  plt.fill_between(
+    coronasurveys_gb['days'],
+    coronasurveys_gb['p_active_low'], # type: ignore
+    coronasurveys_gb['p_active_high'], # type: ignore
+    alpha=0.5,
+    label='UK, active (error)'
   )
   plt.title('Coronasurveys')
   plt.xlabel('Days since 2020/01/06')
@@ -129,6 +143,18 @@ def ons():
     ons_wls['p_infected_high'], # type: ignore
     alpha=0.5,
     label='Wales (error)'
+  )
+  plt.plot(
+    ons_gb['days'],
+    ons_gb['p_infected'],
+    label='UK'
+  )
+  plt.fill_between(
+    ons_gb['days'],
+    ons_gb['p_infected_low'], # type: ignore
+    ons_gb['p_infected_high'], # type: ignore
+    alpha=0.5,
+    label='UK (error)'
   )
   plt.title('ONS')
   plt.xlabel('Days since 2020/01/06')
@@ -273,15 +299,49 @@ def wls():
   plt.xlim(1000)
   plt.ylim(0, 100)
   plt.show()
+def gb():
+  '''UK active cases plot''' 
+  plt.figure('UK Active Cases Data Plot', figsize=(8, 6))
+  plt.plot(
+    coronasurveys_gb['days'],
+    coronasurveys_gb['p_active'],
+    label='Coronasurveys'
+  )
+  plt.fill_between(
+    coronasurveys_gb['days'],
+    coronasurveys_gb['p_active_low'], # type: ignore
+    coronasurveys_gb['p_active_high'], # type: ignore
+    alpha=0.5,
+    label='Coronasurveys (error)'
+  )
+  plt.plot(
+    ons_gb['days'],
+    ons_gb['p_infected'],
+    label='ONS'
+  )
+  plt.fill_between(
+    ons_gb['days'],
+    ons_gb['p_infected_low'], # type: ignore
+    ons_gb['p_infected_high'], # type: ignore
+    alpha=0.5,
+    label='ONS (error)'
+  )
+  plt.title('UK Active Cases')
+  plt.xlabel('Days since 2020/01/06')
+  plt.ylabel('Percentage of cases')
+  plt.legend(loc='upper left')
+  plt.xlim(1000)
+  plt.ylim(0, 10)
+  plt.show()
 
 # show plots
 print('Showing Coronasurveys data from all regions …')
 coronasurveys()
 print('Showing ONS data from all regions …')
 ons()
-print('Regional comparisons compare data across all sources.')
+print('\nRegional comparisons compare data across all sources.')
 print('Coronasurveys data shows percentage of all recorded cases.')
-print('ONS data shows percentage of active cases.')
+print('ONS data shows percentage of active cases.\n')
 print('Comparing England regional data …')
 eng()
 print('Comparing Northern Ireland regional data …')
@@ -290,3 +350,6 @@ print('Comparing Scotland regional data …')
 sct()
 print('Comparing Wales regional data …')
 wls()
+print('\nNow using only active case count data.')
+print('Comparing UK data across all sources …')
+gb()
